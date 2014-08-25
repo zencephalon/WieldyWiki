a6012c12d723633f9eefa0f1c9b787a5e29371f5
Matthew Bunday <zenceph@gmail.com>
2014-08-25 17:06:53 -0400

    Update controller to take wildcard path to Tree

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,12 @@
+
 get '/' do
-  p Tree.first
-  p Tree.first.branches.first
-  @content = Tree.first.branches.first.content
+  tree = Tree.first
+  @url = tree.url
+  @content = tree.branches.first.content
   erb :hello
+end
+
+get '/*' do |url|
+  @tree = Tree.find_by(url: url)
+  erb :tree
 end
