ef952705c29d4115b3d6d307140dc1e14df95a34
Matthew Bunday <zenceph@gmail.com>
2014-08-25 16:24:21 -0400

    Update seeds to seed a tree and branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-Author.create(name: "zen", password: "mindful")
+author = Author.create(name: "zen", password: "mindful")
+tree = author.trees.create(url: "index")
+tree.branches.create(content: "Welcome, I luv U", author: author)
