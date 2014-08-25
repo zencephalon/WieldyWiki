63c14a810e447e4ed326dc721c323eed1a64fb0e
Matthew Bunday <zenceph@gmail.com>
2014-08-25 16:24:08 -0400

    Add belongs_to :author to Branch

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,5 +1,6 @@
 class Branch < ActiveRecord::Base
   belongs_to :tree
+  belongs_to :author
 
   has_many :links_in, as: :link_in, class_name: :Link
   has_many :links_out, as: :link_out, class_name: :Link
