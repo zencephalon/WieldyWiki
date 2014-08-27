aa8aa38dc766849846ff2a32c91b9b40771f944e
Matthew Bunday <zenceph@gmail.com>
2014-08-27 16:00:21 -0400

    Updating seed file to add links.

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,8 +1,10 @@
 class Tree < ActiveRecord::Base
   # Trees don't track links_out, branches do.
   has_many :links_in, as: :link_in, class_name: :Link
+
   has_many :branches
   has_one :branch
+  
   belongs_to :author
 
   has_many :children, class_name: :Tree, foreign_key: :parent_id
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 author = Author.create(name: "zen", password: "mindful")
 tree = author.trees.create(url: "index")
 tree.branches.create(content: "Welcome, I luv U", author: author)
+link = Link.create(link_from: tree.branches.first, link_to: tree)
