2fca191804fd29c53d30117675ad36ed01aec2cf
Matthew Bunday <zenceph@gmail.com>
2014-09-01 12:07:05 -0400

    Luv seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
-author = Author.create(name: "zen", password: "mindful")
-tree = author.trees.create(url: "index")
-tree.branches.create(content: "Welcome, I luv U", author: author)
-link = Link.create(link_from: tree.branches.first, link_to: tree)
+# author = Author.create(name: "zen", password: "mindful")
+# tree = author.trees.create(url: "index")
+# tree.branches.create(content: "Welcome, I luv U", author: author)
+# link = Link.create(link_from: tree.branches.first, link_to: tree)
+
+Luv.create(title: "ILUVU", content: "I luv all of U.")
+Luv.create(title: "Pronoia", content: "Everyone is out to help U.")
