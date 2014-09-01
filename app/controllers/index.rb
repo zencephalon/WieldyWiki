6ac8d08d6a0d19db8a8c110ecdd3ccf7453b6fcf
Matthew Bunday <zenceph@gmail.com>
2014-09-01 11:16:23 -0400

    Clear the way for the luvstream

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,12 @@
 
-get '/' do
-  tree = Tree.first
-  @url = tree.url
-  @content = tree.branches.first.content
-  erb :hello
-end
+# get '/' do
+#   tree = Tree.first
+#   @url = tree.url
+#   @content = tree.branches.first.content
+#   erb :hello
+# end
 
-get '/*' do |url|
-  @tree = Tree.find_by(url: url)
-  erb :tree
-end
+# get '/*' do |url|
+#   @tree = Tree.find_by(url: url)
+#   erb :tree
+# end
