7ccb5fcb9bf9cf14f22a32b024be1b678306cf7e
Matthew Bunday <zenceph@gmail.com>
2014-09-24 10:11:57 -0400

    Use before filter in controller

--- app/controllers/luv.rb
+++ app/controllers/luv.rb
@@ -7,8 +7,12 @@ get '/' do
   erb :'luv/index'
 end
 
-get '/luv/:id' do
+before '/luv/:id' do
   @luv = Luv.find(params[:id])
+end
+
+get '/luv/:id' do
+  #@luv = Luv.find(params[:id])
 
   if params[:ajax]
     @luv.to_json
@@ -22,4 +26,7 @@ post '/luv' do
   @luv = Luv.create(params[:luv])
 
   erb :'luv/single', locals: {luv: @luv}, layout: false
+end
+
+delete '/luv/:id' do
 end
